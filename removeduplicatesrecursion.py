d59759b784036ed3468ede80d4c499cb3fab1ee8
garvit <agarvit1142000@gmail.com>
2022-08-31 23:31:30 +0530

    remove duplicates from all of the array using recursion

--- removeduplicatesrecursion.py
+++ removeduplicatesrecursion.py
@@ -3,10 +3,11 @@ def removeduplicates(l,temp):
     if len(l)==0:
         return l
     
-    elif l[0]==temp:
-        return removeduplicates(l[1:],l[0])
+    elif l[0] in temp:
+        return removeduplicates(l[1:],temp)
 
-    elif l[0]!=temp:
-        return ( [l[0]] + removeduplicates(l[1:],l[0]) )
+    elif l[0] not in temp:
+        temp.append(l[0])
+        return ( [l[0]] + removeduplicates(l[1:],temp) )
 
-print(removeduplicates([1,1,2],-1))
+print(removeduplicates([1,1,2,1],[-1]))
